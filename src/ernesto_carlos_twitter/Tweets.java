b05b80ff395c0af683342de30de7b4d2cfcb0f3f
ErnestoGuevaraBonilla <ernestoguevarab28@outlook.com>
2024-09-15 21:54:24 -0600

    Twitter Final

--- src/ernesto_carlos_twitter/Tweets.java
+++ src/ernesto_carlos_twitter/Tweets.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package proyectoredsocial;
+package ernesto_carlos_twitter;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
